7da6e4cb26ffde96a770726fcc902703b6319e0a
Ylrahc_Play <Mail@without.com>
2018-09-25 22:35:34 +0200

    formate PyQGIS

--- PyQGIS/ManipAutreComposantInterface/SpecifDureeAffichLimitTps.py
+++
@@ -1,5 +0,0 @@
-#-*-coding: utf-8 -*-
-from qgis.gui import QgsMessageBar
-from qgis.utils import iface
-
-iface.messageBar().pushMessage(u"Géotraitement", u"L'opération s'est correctement déroulée ...", level=QgsMessageBar.SUCCESS, duration=4) # en secondes
