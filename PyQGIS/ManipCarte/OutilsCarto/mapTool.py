eda17cbd73e635db59f50efa411e44223a2727c0
Ylrahc_Play <Mail@without.com>
2018-09-25 22:38:36 +0200

    init void

--- PyQGIS/ManipCarte/OutilsCarto/mapTool.py
+++
@@ -1,8 +0,0 @@
-#-*-coding: utf-8 -*-
-from qgis.utils import iface
-
-# Référencer l’outil actif de la carte (il ne peut y en avoir qu’un)
-outilCourant = iface.mapCanvas().mapTool() # renvoie une instance de QgsMapTool
-
-# Changer d’outil courant :
-iface.mapCanvas().setMapTool(unAutreOutil) # pour changer d'outil courant
--- PyQGIS/ManipProjet/RefProjCourant.py
+++
@@ -1,9 +0,0 @@
-#-*-coding: utf-8 -*-
-from qgis.core import QgsProject
-
-leProjet = QgsProject.instance()
-
-# fileName() renvoie :
-# - chaine vide si aucun projet ouvert
-# - chemin complet du fichier projet
-print(leProjet.fileName())
